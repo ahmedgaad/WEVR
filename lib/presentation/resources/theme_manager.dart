3fb0f118ead44f19d844c660bbf6f1278014f916
Mawada Elkashlan <mawadaelkashlan@gmail.com>
2022-10-16 21:40:22 +0200

    updaes

--- lib/presentation/resources/theme_manager.dart
+++ lib/presentation/resources/theme_manager.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wevr_app/presentation/resources/color_manager.dart';
 
-ThemeData getThemeData(){
+ThemeData getThemeData() {
   return ThemeData(
-    //main colors
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.lightPrimary,
+      //main colors
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.lightPrimary,
+      appBarTheme: const AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarBrightness: Brightness.dark,
+              statusBarColor: Colors.transparent)),
   );
 }
--- lib/presentation/splash/splash_view.dart
+++ lib/presentation/splash/splash_view.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wevr_app/presentation/resources/assets_manager.dart';
 import 'package:wevr_app/presentation/resources/color_manager.dart';
 import 'package:wevr_app/presentation/resources/constants_manager.dart';
@@ -7,12 +8,12 @@ import 'package:wevr_app/presentation/resources/routes_manager.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({Key? key}) : super(key: key);
-
   @override
   State<SplashView> createState() => _SplashViewState();
 }
 
 class _SplashViewState extends State<SplashView> {
+
   Timer? _timer;
 
   _startDelay() {
@@ -31,6 +32,13 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: ColorManager.transparent,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
