d0b6f22a7b20086c102616b3f047f55fde86ce35
Ahmed Gad <70586104+ahmedgaad@users.noreply.github.com>
2022-11-16 13:02:14 +0200

    precache image to fast load

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors_in_immutables, depend_on_referenced_packages
 
 import 'package:flutter/material.dart';
+import 'package:wevr_app/shared/managers/assets_manager.dart';
 import 'package:wevr_app/shared/managers/routes_manager.dart';
 import 'package:wevr_app/shared/managers/theme_manager.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,6 +19,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    precacheImage(const AssetImage("assets/images/logo/get_started.jpg"), context);
     return ScreenUtilInit(
       designSize: const Size(360, 900),
       minTextAdapt: true,
