9db5a5b69797a5c69e8f59d44292249e1ba02547
Ahmed Gad <70586104+ahmedgaad@users.noreply.github.com>
2022-12-09 22:48:36 +0200

    create user info screens and routes

---
+++ lib/presentation/screens/my_properties.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyProperties extends StatelessWidget {
+  const MyProperties({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
